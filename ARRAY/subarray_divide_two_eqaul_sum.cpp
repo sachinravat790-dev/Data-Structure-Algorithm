8a5f5f81a4e41073b087436e3c87fd8e126cff43
Sachin Kumar <sachinravat790@gmail.com>
2026-07-25 10:36:15 +0530

    push code

---
+++ ARRAY/subarray_divide_two_eqaul_sum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+bool divide(vector<int> arr)
+{
+    int maxi =INT_MIN  , prefix =0, total_sum=0, n=arr.size();
+    //tota sum 
+    for(int i=0; i<n; i++)
+    total_sum+=arr[i];
+
+    for(int i=0; i<n; i++)
+    {
+        prefix+=arr[i];
+        if(total_sum==2*prefix)
+        return 1;
+    }
+    return 0;
+}
+    int main(){
+        int n;
+        cout<<"enter the size of array:";
+        cin>>n;
+        vector<int>v(n);
+        cout<<"enter the elements in array:";
+        for(int i=0; i<n; i++)
+        cin>>v[i];
+
+        cout<<divide(v);
+    }
