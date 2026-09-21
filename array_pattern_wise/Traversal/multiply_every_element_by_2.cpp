0e3d8c637bc3d0081fd3f8bad4879617cac1c52f
Sachin Kumar <sachinravat790@gmail.com>
2026-09-21 21:13:33 +0530

    push code

---
+++ array_pattern_wise/Traversal/multiply_every_element_by_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size:";
+    cin>>n;
+    cout<<"enter the element:";
+    
+    int arr[n];
+    for(int i=0; i<n; i++)
+    cin>>arr[i];
+
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]*2<<"  ";
+    }
+}
