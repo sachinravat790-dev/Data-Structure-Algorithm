feda2ec9d582e196b8c7e295619137c200a13264
Sachin Kumar <sachinravat790@gmail.com>
2026-09-21 21:17:43 +0530

    push code

---
+++ array_pattern_wise/Traversal/add_five_to_every_element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int sum=0;
+
+    //user input take
+    cout<<"enter the size:";
+    cin>>n;
+
+    cout<<"enter the elements:";
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+    cin>>arr[i];
+
+    for(int i=0;i<n; i++)
+    {
+        cout<<arr[i]+5<<" ";
+    }
+}
