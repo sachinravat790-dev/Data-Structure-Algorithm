20d7eaeb0f58ed17a68ee723e8339a4dd032e808
Sachin Kumar <sachinravat790@gmail.com>
2026-09-22 00:48:07 +0530

    push code

---
+++ array_pattern_wise/Traversal/find_first_element_smaller_than_previous.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size:";
+    cin>>n;
+    cout<<"enter the element:";
+    
+    int arr[n];
+    for(int i=0; i<n; i++)
+    cin>>arr[i];
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            cout<<arr[i];
+            return 0;
+        }
+    }
+    cout<<"not greater than any element";
+}
