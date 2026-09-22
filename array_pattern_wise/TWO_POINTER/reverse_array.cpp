f8430bf9a06d1c6d9f6e0598bc906857cac8ce61
Sachin Kumar <sachinravat790@gmail.com>
2026-09-23 01:38:46 +0530

    push code

---
+++ array_pattern_wise/TWO_POINTER/reverse_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size:";
+    cin>>n;
+    cout<<"enter the element:";
+    
+    int arr[n];
+    for(int i=0; i<n; i++)
+    cin>>arr[i];
+
+    int start=0;
+    int end=n-1;
+
+
+    while(start<end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
